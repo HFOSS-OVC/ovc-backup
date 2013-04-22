38b7276d57ac6f810959cec52106454e2c5ace58
CCoffie <CalebCoffie@gmail.com>
2013-04-22 19:03:40 -0400

    added gobject.threads_init() back in for testing.

--- OpenVideoChat.activity/ovc.py
+++ OpenVideoChat.activity/ovc.py
@@ -50,6 +50,9 @@ class OpenVideoChatActivity(Activity):
     def __init__(self, handle):
         Activity.__init__(self, handle)
 
+        # gobject is used for timeing (will be removed when rtp is implemented)
+        gobject.threads_init()
+        
         # Self-Enforced max_participants
         self.max_participants = 2
 
